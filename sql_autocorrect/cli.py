b7da6defff2588759db5e89f85f7b8dae61efedd
Rafael Lopez <lopez@airelle.info>
2019-12-11 14:46:13 +0100

    Changement de défaut pour le flag -c

--- sql_autocorrect/cli.py
+++ sql_autocorrect/cli.py
@@ -614,7 +614,7 @@ def parse_args(args):
                         help="Base sur laquelle exécuter la requête", metavar='BDD')
     parser.add_argument('-g', default=False, action='store_true',
                         help="Mode note (défaut : non)")
-    parser.add_argument('-c', default=True, action='store_true',
+    parser.add_argument('-c', default=False, action='store_true',
                         help="Mode commentaire (défaut : oui)")
     parser.add_argument('-res', default=False, action="store_true",
                         help="Affiche le résultat de la requête")
