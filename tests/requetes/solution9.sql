dd5e9224312447a7271e01436d796f343011657b
Rafael Lopez <lopez@airelle.info>
2019-12-13 16:54:50 +0100

    Nouvelle requête de test

--- tests/requetes/solution9.sql
+++ tests/requetes/solution9.sql
@@ -6,3 +6,12 @@ AND IdLangage = IdPropriete
 AND VersionJeu.IdVersion = TravailVersion.IdVersion
 AND Conception.IdJeu = VersionJeu.IdJeu
 AND NomPropriete = 'French';
+
+SELECT COUNT(DISTINCT IdConcepteur) AS NbConcepteursVF
+FROM Conception, TravailVersion, Langue, Propriete, VersionJeu
+WHERE IdConcepteur = IdPersonne
+AND TravailVersion.IdVersion = Langue.IdVersion
+AND IdLangage = IdPropriete
+AND VersionJeu.IdVersion = TravailVersion.IdVersion
+AND Conception.IdJeu = VersionJeu.IdJeu
+AND NomPropriete = 'French';
