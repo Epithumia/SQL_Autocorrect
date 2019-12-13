4690db647af7921cd448a2f191e61a2c3af3d6ec
Rafael Lopez <lopez@airelle.info>
2019-12-13 17:13:41 +0100

    Nouvelle requête de test

--- tests/requetes/solution10.sql
+++ tests/requetes/solution10.sql
@@ -1,1 +1,5 @@
-
+SELECT COUNT(*) AS NbJeuxStegmaier, COUNT(Rang) AS NbJeuxClassesStegmaier
+FROM Jeu, Conception, Personne
+WHERE Jeu.IdJeu = Conception.IdJeu
+AND IdConcepteur = IdPersonne
+AND NomPersonne = 'Jamey Stegmaier';
